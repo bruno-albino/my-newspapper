04b30a95b1c3e7e35629b68c367bf45805152cc9
Brunoalbino97 <bruno-albino@hotmail.com>
2021-05-14 12:48:58 -0300

    add default function to get news

--- src/pages/education/index.js
+++ src/pages/education/index.js
@@ -1,29 +1,19 @@
-const getJsonFile = () => {
-  const http = new XMLHttpRequest();
-  http.overrideMimeType('application/json')
-  http.open('GET', '../../configs/news.json', true)
-  http.send()
-
-  return new Promise(resolve => {
-    http.onreadystatechange = () => {
-      if(http.readyState === 4 && http.status == '200') {
-        resolve(http.responseText)
-      }
-    }
-  })
-}
+import * as constants from '../../configs/constants.js'
+import utils from '../../utils.js'
 
 const init = async () => {
-  const response = await getJsonFile()
-  const mainElement = document.getElementById('education')
-  const news = JSON.parse(response).news['education']
-  for(const educationNews of news) {
-    console.log(educationNews)
-    const divElement = document.createElement('div')
-    divElement.className = 'teste'
-    divElement.innerHTML = educationNews.title
-    mainElement.appendChild(divElement)
-  }
+  const news = await utils.getNewsByType(constants.EDUCATION)
+  console.log(news)
+  // const response = await getJsonFile()
+  // const mainElement = document.getElementById('education')
+  // const news = JSON.parse(response).news['education']
+  // for(const educationNews of news) {
+  //   console.log(educationNews)
+  //   const divElement = document.createElement('div')
+  //   divElement.className = 'teste'
+  //   divElement.innerHTML = educationNews.title
+  //   mainElement.appendChild(divElement)
+  // }
 }
 
 init()
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
-import { news } from './configs/news.json'
-
-const getNewsByType = type => {
-  return news[type] || []
+const getNewsByType = async type => {
+  const response = await fetch('../../configs/news.json').then(response => response.json())
+  return response.news[type] || []
 }
 export default {
   getNewsByType
